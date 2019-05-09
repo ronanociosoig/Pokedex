86bdc632e2b74158ee1d830a9ac8ae25243bf66d
Ronan O Ciosoig <ronanoc@icloud.com>
2019-05-09 13:53:12 +0200

    Initial Commit

---
+++ Wefox Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wefox Pokedex
+//
+//  Created by Ronan on 09/05/2019.
+//  Copyright © 2019 Sonomos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
