583ea390d60e91988d30a4d8063fc09ebce65b47
Ronan O Ciosoig <ronanoc@icloud.com>
2019-05-09 18:06:10 +0200

    Add placeholder image for collection view cell.

---
+++ Wefox Pokedex/Scenes/Backpack Scene/Cells/PokemonCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonCollectionViewCell.swift
+//  Wefox Pokedex
+//
+//  Created by Ronan on 09/05/2019.
+//  Copyright © 2019 Sonomos. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
