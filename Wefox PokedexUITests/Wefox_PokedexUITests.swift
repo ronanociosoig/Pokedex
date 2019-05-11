3a5e46f1837a0fff0e83d0b4a6a7768e0b270f1e
Ronan O Ciosoig <ronanoc@icloud.com>
2019-05-11 18:05:09 +0200

    Add UI test to catch a Pokemon, and go to the backpack and show detail.

--- Wefox PokedexUITests/Wefox_PokedexUITests.swift
+++ Wefox PokedexUITests/Wefox_PokedexUITests.swift
@@ -10,25 +10,26 @@ import XCTest
 
 class Wefox_PokedexUITests: XCTestCase {
 
+    let app = XCUIApplication()
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        super.setUp()
         continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launchArguments += ["UITesting"]
+        app.launch()
+        
+        print(XCUIApplication().debugDescription)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSearchPokemon() {
+        let ballButton = app.buttons["Ball"]
+        ballButton.tap()
+        app.alerts["Do you want to leave it or catch it?"].buttons["Catch it!"].tap()
+        ballButton.tap()
+        app.buttons["Backpack"].tap()
+        app.collectionViews.cells.otherElements.containing(.staticText, identifier: "Charmeleon").element.tap()
+        app.navigationBars["Charmeleon"].buttons["Backpack"].tap()
+        app.navigationBars["Backpack"].buttons["Close"].tap()
     }
 
 }
