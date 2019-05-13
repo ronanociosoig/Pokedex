9489953e498da2404a888deaedb028d2b3099ade
Ronan O Ciosoig <ronanoc@icloud.com>
2019-05-14 00:07:02 +0200

    Simplify the Pokemon model by removing unused properties.

--- Wefox Pokedex/Model/Pokemon.swift
+++ Wefox Pokedex/Model/Pokemon.swift
@@ -9,61 +9,22 @@
 import Foundation
 
 struct Pokemon: Codable {
-    let abilities: [Ability]
     let baseExperience: Int
-    let forms: [Species]
-    let gameIndices: [GameIndex]
     let height: Int
-    let heldItems: [HeldItem]
     // swiftlint:disable identifier_name
     let id: Int
     // swiftlint:enable identifier_name
-    let isDefault: Bool
-    let locationAreaEncounters: String
-    let moves: [Move]
     let name: String
     let order: Int
     let species: Species
     let sprites: Sprites
-    let stats: [Stat]
     let types: [TypeElement]
     let weight: Int
-}
-
-struct Ability: Codable {
-    let ability: Species
-    let isHidden: Bool
-    let slot: Int
 }
 
 struct Species: Codable {
     let name: String
     let url: String
-}
-
-struct GameIndex: Codable {
-    let gameIndex: Int
-    let version: Species
-}
-
-struct HeldItem: Codable {
-    let item: Species
-    let versionDetails: [VersionDetail]
-}
-
-struct VersionDetail: Codable {
-    let rarity: Int
-    let version: Species
-}
-
-struct Move: Codable {
-    let move: Species
-    let versionGroupDetails: [VersionGroupDetail]
-}
-
-struct VersionGroupDetail: Codable {
-    let levelLearnedAt: Int
-    let moveLearnMethod, versionGroup: Species
 }
 
 struct Sprites: Codable {
@@ -75,11 +36,6 @@ struct Sprites: Codable {
     let frontFemale: String?
     let frontShiny: String?
     let frontShinyFemale: String?
-}
-
-struct Stat: Codable {
-    let baseStat, effort: Int
-    let stat: Species
 }
 
 struct TypeElement: Codable {
